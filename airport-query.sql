0a522a8fc4fd980a71b3f24b135098d41616c1eb
GCaringi <giannicaringi94@gmail.com>
2022-09-29 09:33:58 +0200

    Add Query with JOIN Without Bonus

--- airport-query.sql
+++ airport-query.sql
@@ -95,3 +95,72 @@ SELECT airline_employee.employee_id
 FROM airline_employee
 GROUP BY airline_employee.employee_id
 HAVING COUNT(airline_employee.employee_id) = 1;
+
+-- Query con JOIN
+-- 1- Selezionare tutti i passeggeri del volo 70021493-2 (85)
+SELECT passengers.*
+FROM passengers
+JOIN flight_passenger ON passengers.id = flight_passenger.passenger_id
+JOIN flights ON flight_passenger.flight_id = flights.id
+WHERE flights.number = '70021493-2'
+-- 2- Selezionare i voli presi da 'Shirley Stokes' (61)
+SELECT flights.*
+FROM flights
+JOIN flight_passenger ON flights.id = flight_passenger.flight_id
+JOIN passengers ON flight_passenger.passenger_id = passengers.id
+WHERE passengers.name = 'Shirley' AND passengers.lastname = 'Stokes'
+-- 3- Selezionare tutti i passeggeri che hanno usato come documento 'Passport'(775)
+SELECT DISTINCT passengers.*
+FROM passengers
+JOIN document_type_passenger ON passengers.id = document_type_passenger.passenger_id
+JOIN document_types ON document_type_passenger.document_type_id = document_types.id
+WHERE document_types.name = 'Passport'
+-- 4- Selezionare tutti i voli con i relativi passeggeri (65296)
+SELECT passengers.name AS "Passengers", flights.id AS "Flight"
+FROM passengers
+JOIN flight_passenger ON passengers.id = flight_passenger.passenger_id
+JOIN flights ON flight_passenger.flight_id = flights.id
+ORDER BY flights.id ASC
+-- 5- Selezionare tutti i voli che partono da 'Charleneland' e arrivano a 'Mauricestad' (3)
+SELECT flights.*
+FROM flights
+JOIN airports dep ON flights.departure_airport_id = dep.id
+JOIN airports arr ON flights.arrival_airport_id = arr.id
+WHERE dep.city = 'Charleneland'AND arr.city = 'Mauricestad'
+-- 6- Selezionare tutti gli id dei voli che hanno almeno un passeggero il cui cognome inizia con 'L' (966) => (935)
+SELECT DISTINCT flights.id
+FROM flights
+JOIN flight_passenger ON flights.id = flight_passenger.flight_id
+JOIN passengers ON flight_passenger.passenger_id = passengers.idS
+WHERE passengers.lastname LIKE 'L%'
+-- 7- Selezionare i dati delle compagnie dove almeno un impiegato si è stato licenziato (286)
+SELECT DISTINCT airlines.*
+FROM employees
+JOIN airline_employee ON employees.id = airline_employee.employee_id
+JOIN airlines ON airline_employee.airline_id = airlines.id
+WHERE airline_employee.layoff_date IS NULL
+-- 8- Selezionare tutti gli aerei che sono partiti almeno una volta dalla città di 'Domingochester' (12) => (14)
+SELECT DISTINCT flights.*
+FROM flights
+JOIN airports ON flights.arrival_airport_id = airports.id
+WHERE airports.city = 'Domingochester'
+-- 9- Selezionare i dati dei tecnici e gli aerei ai quali questi hanno fatto almeno un intervento di manutenzione (1506)
+SELECT employees.*
+FROM employees
+JOIN employee_maintenance_work ON employees.id = employee_maintenance_work.employee_id
+-- 10- Selezionare tutti i piloti che hanno viaggiato nel 2021 verso l'aeroporto di 'Abshireland' (5) => SBAGLIATA LA RICHIESTA
+SELECT employees.*, flights.*
+FROM employees
+JOIN roles on employees.role_id = roles.id
+JOIN employee_flight ON employees.id = employee_flight.employee_id
+JOIN flights ON employee_flight.flight_id = flights.id
+JOIN airports ON flights.arrival_airport_id = airports.id
+WHERE airports.city = 'Abshireland' AND YEAR(flights.departure_datetime) = 2021
+
+-- *********** BONUS ***********
+
+-- 11- Selezionare i dati di tutti i passeggeri che hanno volato su un qualche aereo riparato da 'Aaliyah Leannon' (590)
+
+-- 12- Contare quanti piloti ha la compagnia 'Maldivian (Q2)' (10)
+
+-- 13- Contare quanti dipendenti ha ogni compagnia aerea (286)
